becaf297a477f96f9dd57d424ff36f12cdcab5dd
Pawan <pawankumar131098@gmail.com>
2025-11-15 08:00:18 +0530

    create react app and install tailwind

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="bg-gray-400 h-screen flex justify-center items-center font-bold text-sm">
+      This is Netflix gpt !!
+    </div>
+  );
+}
+
+export default App;
